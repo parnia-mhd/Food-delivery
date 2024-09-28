021cbf09e2694cc57136a02f8dd75f9a79807e8f
parnia-mhd <parnia.mhd80@gmail.com>
2024-09-28 18:19:15 +0330

    header bottom

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,9 @@
 
-// varables
+// variables
 
 let bergerIcon = document.querySelector("#berger-icon")
 let closeIcon = document.querySelector(".closebtn")
+let inputHeader = document.querySelector(".inpt-header-left")
 
 function openNav() {
     document.getElementById("mySidenav").style.transform = "translateX(0)"
@@ -15,4 +16,18 @@ function closeNav() {
     closeIcon.style.display = "none"
     bergerIcon.style.display = "block"
 
+}
+
+// header bottom - left button 
+let inputValue = 0
+inputHeader.value = inputValue
+// plus
+function addNum() {
+    let resultinput = inputHeader.value++
+}
+// minusNum
+function minusNum() {
+    if (inputHeader.value > 0) {
+        let resultinput = inputHeader.value--
+    }
 }
